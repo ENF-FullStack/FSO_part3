ee2d599022df09b936edaa9c4ca2b747735ece79
VP Tervonen <elamshin@protonmail.com>
2022-09-20 05:44:23 +0300

    3.5 ja 3.6 luotu

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let persons = [
     {
@@ -56,6 +57,37 @@ app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(person => person.id !== id)
 
     res.status(204).end()
+})
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * 10000)
+    return id
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    body.id = generateId()
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    if (persons.find((person) => person.name === body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 const PORT = 3001
