73bcd141a028dc196901aaaddc6c8ea383deabd4
VP Tervonen <elamshin@protonmail.com>
2022-09-20 04:00:27 +0300

    3.3 luotu

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -37,6 +38,17 @@ app.get('/info', (req, res) => {
         `<p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date().toString()}</p>`
     )
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 const PORT = 3001
