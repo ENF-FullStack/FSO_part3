9f06c7d1736d828513797d53efb7baf406d4b41d
VP Tervonen <elamshin@protonmail.com>
2022-09-23 16:33:37 +0300

    3.13 luotu

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
+const Person = require('./models/person')
 
 //app.use(morgan('tiny'))
-app.use(morgan(':method :url :status :res[content-length] :response-time ms :body')
-  )
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
+// NOTE: muista poistaa passu ennen git commitia
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        // console.log(persons)
+        res.json(persons)
+    })
+    mongoose.connection.close()
 })
 
 app.get('/info', (req, res) => {
@@ -99,7 +84,7 @@ app.post('/api/persons', (req, res) => {
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
